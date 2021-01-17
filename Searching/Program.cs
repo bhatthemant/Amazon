93be978de6ce7d7bc51bdb38e631cefdd1551d73
hemant bhatt <bhatthemant@live.com>
2021-01-17 20:10:11 +0530

    Driver method.

--- Searching/Program.cs
+++ Searching/Program.cs
@@ -6,7 +6,17 @@ namespace Searching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] inputArr = {10, 20, 80, 30, 60, 50,
+                     110, 100, 130, 170};
+            int element = 110;
+
+            Console.WriteLine($"Element found at {LinearSearch.Search(inputArr, element)}");
+            Console.WriteLine($"Element found at {LinearSearch.SearchImproved(inputArr, element)}");
+
+            element = 175;
+
+            Console.WriteLine($"Element found at {LinearSearch.Search(inputArr, element)}");
+            Console.WriteLine($"Element found at {LinearSearch.SearchImproved(inputArr, element)}");
         }
     }
 }
